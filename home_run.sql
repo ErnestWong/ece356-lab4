c0d8b6f7c6a7206131c5be03651f413505e48990
Ernest Wong <e56wong@ecelinux4.uwaterloo.ca>
2016-11-29 17:12:40 -0500

    updated homerun

--- home_run.sql
+++ home_run.sql
@@ -1,11 +1,12 @@
-(SELECT "yearID", "G", "R", "H", "2B", "3B", "HR", "salary", "playerID", "weight", "height", "bats", "debut")
+(SELECT "yearID", "G", "R", "H", "2B", "3B", "HR", "salary", "playerID", "weight", "height", "bats", "debut", "AB", "SO", "BB", "IBB", "SH", "SF", "GIDP", "birthYear")
 UNION
 (SELECT Batting.yearID, G, R, H, 2B, 3B, HR,
        salary,
-       Master.playerID, weight, height, bats, debut
+       Master.playerID, weight, height, bats, debut, 
+       AB, SO, BB, IBB, SH, SF, GIDP, birthYear
 FROM Batting
 JOIN Salaries USING(yearID, playerID, lgID)
-LEFT JOIN Master USING(playerID)
+LEFT JOIN Master USING(playerID);
 INTO OUTFILE "/Users/peterchu/test.csv"
 FIELDS ENCLOSED BY '"'
 TERMINATED BY ';'
